4837b6966a8f5f4362a46d3202c0ded90e0221de
andsnk <127045868+andsnk@users.noreply.github.com>
2023-09-09 12:16:54 +0300

    add styles task 3

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
+import css from './FriendListItem.module.css';
 export const FriendListItem = ({ friend }) => {
   const { avatar: AVATAR, name, isOnline } = friend;
   return (
-    <li className="item">
-      {/* <span class={isOnline ? css.online : css.offline}></span> */}
-      <span className="">{isOnline ? 'online' : 'offline'}</span>
-      <img className="avatar" src={AVATAR} alt={name} width="48" />
-      <p className="name">{name}</p>
+    <li className={css.item}>
+      <span class={isOnline ? css.online : css.offline}></span>
+      {/* <span className="">{isOnline ? 'online' : 'offline'}</span> */}
+      <img className={css.avatar} src={AVATAR} alt={name} width="48" />
+      <p className={css.name}>{name}</p>
     </li>
   );
 };
